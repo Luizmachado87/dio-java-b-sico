8682f1dfbf5292f2268740562cf774b935c345e7
Luizmachado87 <luizmachado87@outlook.com>
2024-06-12 23:23:21 -0300

    test

--- my-first-project-java/src/App.java
+++ my-first-project-java/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello guys, my name is Luiz. I am student.");
+        System.out.println("Hello guys, my name is Luiz. I am student and you?");
+
     }
 }
